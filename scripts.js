bfbb1ad02a88b25815d4348524ceddfae607ba94
Daniel Marino <starzonmyarmz@gmail.com>
2016-07-31 09:43:58 -0400

    Add some functionality and style

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-console.log("hi")
+(function () {
+
+  var currentSlideClass = "slide-current";
+
+  $("section:first-child").addClass(currentSlideClass);
+
+  $(".js-next").on("click", function() {
+    var current = $("." + currentSlideClass);
+
+    if (current.next()[0].nodeName === "SECTION") {
+      current.removeClass(currentSlideClass).next().addClass(currentSlideClass);
+    }
+  });
+
+  $(".js-prev").on("click", function() {
+    var current = $("." + currentSlideClass);
+
+    if (current.prev()[0].nodeName === "SECTION") {
+      current.removeClass(currentSlideClass).prev().addClass(currentSlideClass);
+    }
+  });
+
+
+})();
